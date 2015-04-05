3fe5f6524d7f03fb282d6b073f1af09a3641c7cc
vinceMeens <v.w.a.meens@comcordis.com>
2015-04-05 23:46:03 +0200

    Minor fixes

--- initialize.php
+++ initialize.php
@@ -3,11 +3,4 @@
 require_once 'XXX_MailGunAPIHelpers.php';
 require_once 'XXX_MailGunAPI_SendEmailService.php';
 
-
-$winPath = 'mailgun-php\\src\\Mailgun\\Mailgun.php';
-$linuxPath = 'mailgun-php/src/Mailgun/Mailgun.php';
-
-
-
-
 ?>
